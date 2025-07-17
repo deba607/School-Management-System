05bafd0cdb4be331a80bacee7270d371054b99d4
Debanjan Paul <128419695+deba607@users.noreply.github.com>
2025-07-17 21:08:20 +0530

    Delete School_Management_System directory

--- School_Management_System/server/router/attendanceStudentRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const attendanceController = require('../controllers/attendanceStudentController');
-
-router.post('/', attendanceController.enrollStudent);
-router.put('/:id', attendanceController.updateStudent);
-router.get('/', attendanceController.getAllStudent);
-
-module.exports = router;
--- School_Management_System/server/router/studentForgotPassword.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/studentForgotPasswordController');
-
-router.post('/send-otp', controller.sendOtp);
-router.post('/verify-otp', controller.verifyOtp);
-router.post('/reset', controller.resetPassword);
-
-module.exports = router;
