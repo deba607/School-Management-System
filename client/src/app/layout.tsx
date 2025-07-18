44c046414f5604f49cc6b1c1f6b2c3fa35f17e12
Debanjan Paul <debanjanpaul607@gmail.com>
2025-07-18 20:27:46 +0530

    Initial Commit in the Debanjan_SMS_Next branch 18/07/2025 8.30 PM

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "School_Management_System",
+  description: "School_Management_System is a comprehensive platform for managing school operations, including student enrollment, attendance tracking, and academic performance monitoring.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
